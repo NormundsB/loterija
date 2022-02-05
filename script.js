6c146f644f0b65fcb43b4f714a0f02a80f104ca2
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-02-05 15:55:19 +0200

    ss

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const vardi = ["Jana Krūmiņa", "Jānis Bērziņš", "Pēteris Pētersons", "Līga Priedīte", "Maija Liepa"];
+const vardi = ["Jana Krūmiņa", "Jānis Bērziņš", "Pēteris Pētersons", "Līga Priedīte", "Maija Liepa", "Juris Pupa", "Jānis Joņins", "Imants Ziedonis", "Renārs Jankovskis", "Kaspars Fridrihsons"];
 const balvas = ["Ledusskapis", "Fotoaparāts", "Kafijas automāts"];
 const naudaKopa = 1000000;
 
 let uzvaretajuSkaits = 3;
+
+let rand = Math.random() * vardi.length;
+rand = Math.floor(rand);
+
+console.log(vardi[rand])
