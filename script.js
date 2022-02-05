6a86dc2e7a20832cbaf3a449d5dd159160ea4958
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-02-05 18:17:25 +0200

    gala

--- script.js
+++ script.js
@@ -5,21 +5,24 @@ const naudaKopa = 1000000;
 let uzvaretajuSkaits = 3;
 let rindas = document.querySelector(".rindas");
 //rindas.innerHTML = "Janis"
+
 //random funkcija param maxCipars
 function randFunc(maxCipars) {
     let random1 = Math.floor(Math.random() * maxCipars)
     return random1
 }
+function izlozet() {
+    rindas.innerHTML = "";
+    for (let i = 0; i < uzvaretajuSkaits; i++) {
 
-for (let i = 0; i < uzvaretajuSkaits; i++) {
+        let rand = randFunc(vardi.length);
 
-    let rand = randFunc(vardi.length);
-
-    let uzvaretajs = vardi[rand];
-    rindas.innerHTML += `
+        let uzvaretajs = vardi[rand];
+        rindas.innerHTML += `
     <tr>
     <td>${i + 1}</td>
     <td>${uzvaretajs}</td>
     </tr>
     `;
-}
+    }
+}//beidzas izlozēt
